54599e6d6c1c0f7599941f4c3402bc536c2b9943
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-24 10:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function threeSum(arr, target) {
     Arrays.sort(arr);
     let res = arr[0] + arr[1] + arr[2];
-	
     for (let i = 0; i < arr.length - 2; i++) {
         let left = i + 1;
         let right = arr.length - 1;
-		
         while (left < right) {
             let Closestsum = arr[i] + arr[left] + arr[right];
-
             if (Math.abs(Closestsum - target) < Math.abs(res - target)) {
                 res = Closestsum;
             }
@@ -23,5 +20,4 @@ function threeSum(arr, target) {
     }
     return res;
 }
-
 module.exports = threeSum;

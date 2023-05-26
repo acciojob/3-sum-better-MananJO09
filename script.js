2a95bf905aaffcf804424f1a661cc80fa51ed966
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-26 10:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-import java.util.Arrays;
-
-public class ThreeSum {
+function threeSum(arr, target) {
     public static int threeSum(int[] arr, int target) {
-        Arrays.sort(arr);
-        int res = arr[0] + arr[1] + arr[2];
+      arr.sort((a, b) => a - b);
+        let res = arr[0] + arr[1] + arr[2];
         
         for (int i = 0; i < arr.length - 2; i++) {
-            int left = i + 1;
-            int right = arr.length - 1;
-            
+            let left = i + 1;
+            let right = arr.length - 1;
             while (left < right) {
-                int closestSum = arr[i] + arr[left] + arr[right];
-                
+                let closestSum = arr[i] + arr[left] + arr[right];
                 if (Math.abs(closestSum - target) < Math.abs(res - target)) {
                     res = closestSum;
-                }
-                
+                }  
                 if (closestSum < target) {
                     left++;
                 } else if (closestSum > target) {
@@ -25,7 +20,6 @@ public class ThreeSum {
                 }
             }
         }
-        
         return res;
     }
 }
